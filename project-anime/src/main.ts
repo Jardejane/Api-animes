01c04794a7c03f7fb3e73cc445745556649f9f25
jardejane <janefeitosalima49@gmail.com>
2022-11-01 13:40:29 -0300

    deleting env

--- project-anime/src/main.ts
+++ project-anime/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.Port || 4000);
+  await app.listen(4000);
 }
 bootstrap();
